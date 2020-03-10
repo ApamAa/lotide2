d909a7e24e237b1af9c9a9eafd604a5bf9e63dda
ApamAa <apama.jamshidi@gmail.com>
2020-03-10 23:06:34 +0000

    debugged the code

--- reverse.js
+++ reverse.js
@@ -1,12 +1,12 @@
+const listOfWord = process.argv.slice(2);
+
 const reverse = function(word) {
-  let newWord = '';
-  for (let i = word.length - 1; i >= 0; i--) {
-    newWord += (word[i]);
+  const words = word.join(" ");
+  let newWord = "";
+  for (let i = words.length - 1; i >= 0; i--) {
+    newWord += words[i];
   }
   return newWord;
 };
 
-
-console.log(reverse('apama'));
-console.log(reverse('gorg'));
-console.log(reverse('lalaland'));
+console.log(reverse(listOfWord));
