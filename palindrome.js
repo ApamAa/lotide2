b36e89fa526c714abdd44b5faa9c93473a94567e
ApamAa <apama.jamshidi@gmail.com>
2020-03-12 13:57:04 +0000

    debug

---
+++ palindrome.js
@@ -0,0 +1,30 @@
+const isPalindrome = function(str) {
+  const noSpaces = str.split(" ").join("");
+  const midIndex = Math.floor(noSpaces.length / 2);
+  const lastIndex = noSpaces.length - 1;
+
+  for (let i = 0; i < midIndex; i++) {
+    if (noSpaces[i] !== noSpaces[lastIndex - i]) return false;
+  }
+  return true;
+};
+
+const assertPalindrome = function(word, expected) {
+  console.log(`Testing isPalindrome(\"${word}\"):`);
+  const actual = isPalindrome(word);
+  if (actual === expected) {
+    console.log("\x1b[32m%s\x1b[0m", `\tPASS ✅ function returned ${actual}\n`);
+  } else {
+    console.log(
+      "\x1b[31m%s\x1b[0m",
+      `\tFAIL 🛑 function returned ${actual} (expected ${expected})\n`
+    );
+  }
+};
+
+assertPalindrome("p", true);
+assertPalindrome("racecar", true);
+assertPalindrome("my gym", true);
+assertPalindrome("foo", false);
+assertPalindrome("fluff", false);
+assertPalindrome("just some random words", false);
